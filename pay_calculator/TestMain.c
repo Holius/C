4f1bac6c64f7ba13e4e50035dffe0a95d18bbd80
Justin Gilley <justindgilley@gmail.com>
2023-05-17 23:12:43 -0500

    dynamic stdout redirection

--- pay_calculator/TestMain.c
+++ pay_calculator/TestMain.c
@@ -1,5 +1,6 @@
 #include "unity.h"
 #include "main.h"
+#include <unistd.h> //dup
 #include <string.h> //strcpy
 #include <stdio.h> //freopen
 
@@ -12,15 +13,21 @@ void test_main(void) {
         "39"
     };
 
-    write = freopen("test_run.txt", "w", stdout);
+    int original_stdout = dup(1);
+    printf("\n");
+    write = fopen("test_run.txt", "w");
+    dup2(fileno(write), 1);
     _main(3, argv);
-    fclose(write);
-    freopen ("/dev/tty", "a", stdout); // restore stdout
+    dup2(original_stdout, 1);
+    close(original_stdout);
     read = fopen("test_run.txt", "r");
     char* expected_stdout = "Gross pay is 468.00\nTax deductions is 79.50\nNet pay is 388.50\n";
     while(ch = getc(read)) { 
         if (ch == EOF) break;
         TEST_ASSERT_EQUAL_UINT8(expected_stdout[index++], ch);
+    }
+    if (index == 0) {
+        TEST_FAIL()
     }
     fclose(read);
 }
